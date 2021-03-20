9f834a1c3e3765178007dd5bf9f1eae1b2020327
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2021-03-20 22:37:20 +0530

    Add files via upload

---
+++ Folder/JSF.js
@@ -0,0 +1,37 @@
+function initViz() {
+    var containerDiv = document.getElementById("viz1"),
+    url = "https://public.tableau.com/views/CatsDogs_16160903457520/Sheet4?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
+
+    var viz = new tableau.Viz(containerDiv, url);
+}
+function initViz2() {
+    var containerDiv2 = document.getElementById("viz2"),
+    url = "https://public.tableau.com/views/CatsDogs_16160903457520/Sheet2?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
+
+    var viz = new tableau.Viz(containerDiv2, url);
+}
+function initViz3() {
+    var containerDiv3 = document.getElementById("viz3"),
+    url = "https://public.tableau.com/views/CatsDogs_16160903457520/Sheet3?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
+
+    var viz = new tableau.Viz(containerDiv3, url);
+}
+window.onload = function(){
+    initViz();
+    initViz2();
+    initViz3();
+}
+
+const TileP = [15, 20, 25, 30];
+let TotalP = [0, 0, 0, 0];
+function KClick(val,ind){
+    TotalP[ind] = val*TileP[ind];
+    idd = "price"+String(ind);
+    document.getElementById(idd).innerHTML=(TotalP[ind]);
+}
+function KFinal(){
+    var sum1 = TotalP.reduce(function(a,b){return a+b;},0);
+    document.getElementById("FinalP").innerHTML=(sum1);
+}
+//for(var i=0; i<4; i++){
+document.getElementById('A'+String(2)).innerHTML = TileP[2];
